91a8d766b5065e2a0dc15419a57a27ff23b83a31
Thiwer <danirivgar@gmail.com>
2018-06-04 21:07:44 +0200

    Commint inicial

---
+++ carta.adb
@@ -0,0 +1,7 @@
+package body carta is
+   mk: constant key_component:= key_componen'first;
+   lastc: constant key_component:= key_component'last;
+   i0: constant key_index := key_index'first;
+
+
+end carta;
---
+++ carta.ads
@@ -0,0 +1,38 @@
+generic
+   type key_component is (<>); -- tipo discreto componentes
+   type key_index is range <>; -- rango de enteros, indice sobre la clave
+   type key is array(key_index) of key_component;
+
+package carta is
+   espacio_desbordado: exception;
+
+   procedure carta_buida (s: out carta);
+   -- Introduce un plato en la carta
+   procedure posar_element (c: in out carta; cat : in tcategoria; k: in tcodi; nom: in Unbounded_String);
+   -- Imprime todos los elementos de una categoria ordenados por codigo
+   procedure imprimir_elements(c : in carta; cat : in tcategoria);
+   -- Elimina un plato de la carta
+   procedure eliminar_element (c: in out carta; cat : in tcategoria; k: in tcodi);
+   -- Introduce un comentario
+   procedure posar_comentari(c : in out carta; cat : in tcategoria; k : in tcodi; q : in tqualificacio; comentari : in Unbounded_String);
+   -- Devuelve un comentario
+   function consulta_comentari(c : in out carta; cat : in tcategoria; k : in tcodi; q : in tqualificacio) return Unbounded_String);
+   -- Existe un comentario
+   function existeix_comentari(c : in carta; cat : in tcategoria; k : in tcodi; q : in tqualificacio) return Boolean;
+   -- Elimina un comentario
+   procedure eliminar_comentari (c : in out carta; cat : in tcategoria; k : in tcodi; cal : in tqualificacio) ;
+
+   -- function existe (s: in carta; x: in elem) return boolean;
+   -- function esta_buida(s: in carta) return boolean;
+
+
+
+private
+   type nodo;
+   type pnodo is access nodo;
+type nodo is array(key_component) of pnodo;
+   type carta is
+      record
+         raiz: pnodo;
+      end record;
+end carta;
